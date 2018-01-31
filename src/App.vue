f72026c83082f68d4051a811f85e492bcbf29b3d
LRH1993 <799386120@qq.com>
2018-01-31 15:06:08 +0800

    侧边栏

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <header class="header">
-      <i v-if="!flag" class="iconfont icon-ic_menu" @click="toggle(true)"></i>
+      <i v-if="!flag" class="iconfont icon-ic_menu" @click="toggle()"></i>
       <i v-if="flag" class="iconfont icon-ic_back" @click="back()"></i>
     </header>
     <aside class="aside" :class="{open:open,docked:docked}" @click="toggle()">
@@ -19,43 +19,142 @@
           <i class="iconfont icon-github"></i>
         </li>
       </ul>
+     <div class="cover" @touchmove.prevent></div>
     </aside>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
-import api from './api/index'
+import api from "./api/index";
 export default {
   data() {
     return {
       open: false,
       docked: false,
-      list:[]
+      list: []
     };
   },
-  mounted () {
+  mounted() {
     let vue = this;
-    api.getTopics().then(data=>{
+    api.getTopics().then(data => {
       vue.list = data.data.others;
-    })
+    });
   },
   computed: mapState({
-    flag: state => state.drawer
+    flag: state => state.drawer,
+    num: state => state.num
   }),
   methods: {
     back(n) {},
-    toggle() {},
-    change(id){
-
+    toggle() {
+      if (!this.open) {
+        this.open = true;
+        this.docked = true;
+      } else {
+        this.open = false;
+        let vue = this;
+        setTimeout(function() {
+          vue.docked = false;
+        }, 300);
+      }
     },
-    jump(){
+    change(id) {},
+    jump() {
       window.open("https://github.com/LRH1993/daily-zhihu");
     }
   }
 };
 </script>
 
-<style>
-
+<style lang="less">
+.header {
+  width: 100%;
+  height: 1.5rem;
+  z-index: 9;
+  padding-left: 5%;
+  background-image: linear-gradient(
+    0deg,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0.51) 95%
+  );
+  .iconfont {
+    color: #fff;
+    font-size: 0.8rem;
+    top: 20%;
+    position: relative;
+  }
+}
+.aside {
+  z-index: 11;
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  visibility: hidden;
+  &::-webkit-scrollbar {
+    display: none !important;
+    width: 0 !important;
+    height: 0 !important;
+    -webkit-appearance: none;
+    opacity: 0 !important;
+  }
+  ul {
+    margin: 0;
+    float: left;
+    width: 60%;
+    height: 100%;
+    padding: 1.3rem 0.5rem 0.5rem;
+    overflow: auto;
+    background: rgba(91, 116, 146, 0.75);
+    transform: translate(-100%, 0);
+    transition: transform 0.3s ease;
+    -webkit-overflow-scrolling: touch;
+    &::-webkit-scrollbar {
+      display: none !important;
+      width: 0 !important;
+      height: 0 !important;
+      -webkit-appearance: none;
+      opacity: 0 !important;
+    }
+  }
+  li {
+    cursor: pointer;
+    font-size: 0.43rem;
+    list-style: none;
+    color: #fff;
+    margin-top: 20px;
+    .iconfont {
+      float: right;
+      font-size: 0.6rem;
+    }
+    &.chose {
+      color: #ffd300;
+    }
+  }
+  .cover {
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    display: none;
+    background: rgba(172, 185, 201, 0.4);
+    transition: opacity 0.3s esse;
+  }
+  &.open {
+    ul {
+      transform: translate(0);
+    }
+    .cover {
+      opacity: 1;
+    }
+  }
+  &.docked {
+    visibility: visible;
+    .cover {
+      display: block;
+    }
+  }
+}
 </style>
+
